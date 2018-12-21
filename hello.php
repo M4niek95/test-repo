800063e6fd8a942a0b74c4ee977ab17c5496254e
Mariusz <uczen6465@wp.pl>
2018-12-21 19:56:34 +0100

    Warunek

--- hello.php
+++ hello.php
@@ -5,4 +5,12 @@ $l1 = 10;
 $l2 = 20;
 $l3 = $l1 + $l2;
 echo $l3;
+echo "<br>";
+$z = -1;
+if ($z > 0) {
+    echo "Zmienna jest większa od 0";
+} else {
+    echo "Zmienna jest mniejsza od 0.";
+}
+
 ?>
