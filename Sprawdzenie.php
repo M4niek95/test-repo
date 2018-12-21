64d78ceb3e203a5861c890115d80fb2a3b31f192
Mariusz <uczen6465@wp.pl>
2018-12-21 19:45:42 +0100

    my first repo

---
+++ Sprawdzenie.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Sprawdzanie GITa</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="GIT">
+    <meta name="description" content="Sprawdzanie działania GITa">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header><h1>Tytul strony</h1> </header>
+    
+    <div id="1"> 
+    <?php include('hello.php'); ?>
+    </div>
+    <br><br>
+<p>sdbfkdbsfkjdbsfkdbsfkdbksdbfksdbfjlsadflasiwaehgfaiLFAJSDBFLIASDBFADBGL
+    ADGBDAFGAOUI ATGRL AUYGAKUDGFKAFGK AU UY kuy uyk gygkdfgdu</p>
+</body>
+</html>
---
+++ hello.php
@@ -0,0 +1,8 @@
+<?php 
+echo "Hello Word";
+echo "<br>";
+$l1 = 10;
+$l2 = 20;
+$l3 = $l1 + $l2;
+echo $l3;
+?>
