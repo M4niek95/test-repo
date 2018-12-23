d559fc4188ba8ed6f6686caf705f54b95bfe7b63
Mariusz <uczen6465@wp.pl>
2018-12-23 17:43:00 +0100

    Obliczanie delty

--- hello.php
+++ hello.php
@@ -12,5 +12,46 @@ if ($z > 0) {
 } else {
     echo "Zmienna jest mniejsza od 0.";
 }
+echo "<br>";
+echo "<br>";
+echo "<br>";
+$liczba = 20;
+if ($liczba == 10) {
+    echo "Nasza zmienna jest równa 10";
+} else if ($liczba == 20) {
+    echo "Nasza zmienna jest równa 20";
+} else if ($liczba == 30) {
+    echo "Nasza zmienna jest równa 30";
+}
+echo "<br>";
+echo "<br>";
+echo "Obliczanie delty";
+echo "<br>";
+$A = -10;
+$B = 12; 
+$C = 10;
+echo "Parametry równiania: <br>";
+echo "A = $A, B = $B, C = $C<br>";
+if ($A == 0) {
+    echo "To nie jest równanie kwadratowe. A = 0!";
+} else {
+    $delta = $B ** 2 - 4 * $A * $C;
+   if ($delta < 0) {
+       echo "Delta < 0 <br>";
+       echo "Brak rozwiązań!<br>";
+   } else {
+       if ($delta == 0) {
+           $wynik = -$B / ( 2 * $A );
+           echo "Rozwiązanie: x = $wynik";
+       
+   } else {
+       $wynik = (-$B + sqrt($delta)) / (2*$A);
+       echo "Rozwiązanie: x1 = $wynik";
+       $wynik = (-$B - sqrt($delta)) / (2*$A);
+       echo " , x2 = $wynik";
+        }
+    }
+}
+
 
 ?>
